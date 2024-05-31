931f181094fa22298260fcf6b99c7c2a33185b7c
Ilija Pavlovic <ilija.pavlovic05@gmail.com>
2024-06-01 00:22:29 +0200

    Application is done

---
+++ B04/Pages/oautoru.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace B04.Pages
+{
+    public class oautoruModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
